7fc2d2d23ee7f62a3fc383c8f599ac385964bed8
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-02-13 15:21:44 +0100

    Added a counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, H1 } from "collector-portal-framework/dist/components";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "collector-portal-framework";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Container = styled.div({
+  display: "flex"
+});
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>Babby's first stateful componet</p>
+
+        <Container>
+          <Button size="small" onClick={() => setCount(prev => prev - 1)}>
+            Minska
+          </Button>
+
+          <H1 style={{ margin: 16 }} centered>
+            {count}
+          </H1>
+
+          <Button size="small" onClick={() => setCount(prev => prev + 1)}>
+            Öka
+          </Button>
+        </Container>
+      </header>
+    </div>
+  );
+};
 
 export default App;
