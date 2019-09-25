ce89a94fa8d9c35132243057a13a888d5c70ec7a
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-09-25 11:26:41 +0200

    Last minute fixes

--- src/views/AgeOfEmpires.tsx
+++ src/views/AgeOfEmpires.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { PageContainer, PageHeader, FetchHandler, ListItem, TextLink } from 'collector-portal-framework/dist/components';
+import React, { useState } from 'react';
+import { PageContainer, PageHeader, FetchHandler, ListItem, TextLink, Paginator } from 'collector-portal-framework/dist/components';
 import { getCivilizations } from '../api';
 import styled from 'collector-portal-framework';
 
@@ -12,34 +12,50 @@ const DetailsContainer = styled.div(({ theme }) => ({
 const List = styled.ul({
     margin: 0,
     padding: 0,
+    marginBottom: 32,
 });
 
-export const AgeOfEmpires: React.FC = () => (
-    <>
-        <PageHeader title="Age of Empires API Example" />
+export const AgeOfEmpires: React.FC = () => {
+    const [activePage, setActivePage] = useState(1);
+    const pageSize = 5;
 
-        <PageContainer>
-            <FetchHandler apiMethod={getCivilizations} errorText="Need more wood. Wololo!">
-                {response => (
-                    <List>
-                        <span>Gratefully fetched from </span>
-                        <TextLink href="https://age-of-empires-2-api.herokuapp.com/docs">Age of Empires II Api</TextLink>
+    const renderDetails = (civ: Swagger.Civilization): JSX.Element => (
+        <ListItem key={civ.id} item={{ title: civ.name, subTitle: civ.team_bonus, details: civ.expansion }}>
+            <DetailsContainer>
+                <List>
+                    {civ.civilization_bonus &&
+                        civ.civilization_bonus.map(bonus => <ListItem key={`${Math.random()}`} item={{ title: bonus }} />)}
+                </List>
+            </DetailsContainer>
+        </ListItem>
+    );
 
-                        {response.civilizations.map((civ, index) => renderDetails(civ, response.civilizations, index))}
-                    </List>
-                )}
-            </FetchHandler>
-        </PageContainer>
-    </>
-);
+    return (
+        <>
+            <PageHeader title="Age of Empires API Example" />
 
-const renderDetails = (civ: Swagger.Civilization, civilizations: Swagger.Civilization[], index: number): JSX.Element => (
-    <ListItem key={civ.id} item={{ title: civ.name, subTitle: civ.team_bonus, details: civ.expansion }}>
-        <DetailsContainer>
-            <List>
-                {civ.civilization_bonus &&
-                    civ.civilization_bonus.map(bonus => <ListItem key={civilizations.length + index} item={{ title: bonus }} />)}
-            </List>
-        </DetailsContainer>
-    </ListItem>
-);
+            <PageContainer>
+                <FetchHandler apiMethod={getCivilizations} errorText="Need more wood. Wololo!">
+                    {response => (
+                        <>
+                            <List>
+                                <span>Gratefully fetched from </span>
+                                <TextLink href="https://age-of-empires-2-api.herokuapp.com/docs">Age of Empires II Api</TextLink>
+
+                                {response.civilizations.slice((activePage - 1) * pageSize, activePage * pageSize).map(renderDetails)}
+                            </List>
+
+                            <Paginator
+                                activePage={activePage}
+                                numberOfItems={response.civilizations.length}
+                                numbersInMiddle={5}
+                                onChange={setActivePage}
+                                pageSize={pageSize}
+                            />
+                        </>
+                    )}
+                </FetchHandler>
+            </PageContainer>
+        </>
+    );
+};
