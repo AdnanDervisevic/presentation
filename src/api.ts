8c13f4da34c4c38137c58ef5c3aa501d66f5f4ac
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-09-25 10:00:49 +0200

    Add endpoint for live coding fun

--- src/api.ts
+++ src/api.ts
@@ -2,3 +2,6 @@ import axios from 'axios';
 
 export const getCivilizations = (): Promise<Swagger.CivilizationList> =>
     axios.get(`https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations`).then(response => response.data);
+
+export const getUnits = (): Promise<Swagger.UnitList> =>
+    axios.get(`https://age-of-empires-2-api.herokuapp.com/api/v1/units`).then(response => response.data);
