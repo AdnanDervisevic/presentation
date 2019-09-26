a66237321e5086c050722244cc14e921c33c9eb6
Adnan Dervisevic <adnan.dervisevic@collectorbank.se>
2019-09-26 09:15:53 +0200

    Clean up inline styles

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,30 +1,40 @@
-import React, { useState } from 'react';
-import { Button, H1, ButtonGroup } from 'collector-portal-framework/dist/components';
-import styled from 'collector-portal-framework';
+import React, { useState } from "react";
+import {
+  Button,
+  H1,
+  ButtonGroup
+} from "collector-portal-framework/dist/components";
+import styled from "collector-portal-framework";
 
 const Container = styled.div({
-    display: 'flex',
-    alignItems: 'center',
+  display: "flex",
+  alignItems: "center"
+});
+
+const StyledButtonGroup = styled(ButtonGroup)({
+  height: "100%"
+});
+
+const CounterLabel = styled(H1)({
+  margin: 16,
+  minWidth: 50,
+  textAlign: "center"
 });
 
 export const Counter: React.FC = () => {
     const [count, setCount] = useState(0);
 
-    return (
-        <Container>
-            <ButtonGroup style={{ height: '100%' }}>
-                <Button size="small" onClick={() => setCount(prev => prev - 1)}>
-                    Minska
-                </Button>
-            </ButtonGroup>
+  return (
+    <Container>
+      <StyledButtonGroup>
+        <Button onClick={() => setCount(prev => prev - 1)}>Minska</Button>
+      </StyledButtonGroup>
 
-            <H1 style={{ margin: 16, minWidth: 50, textAlign: 'center' }}>{count}</H1>
+      <CounterLabel>{count}</CounterLabel>
 
-            <ButtonGroup style={{ height: '100%' }}>
-                <Button size="small" onClick={() => setCount(prev => prev + 1)}>
-                    Öka
-                </Button>
-            </ButtonGroup>
-        </Container>
-    );
+      <StyledButtonGroup>
+        <Button onClick={() => setCount(prev => prev + 1)}>Öka</Button>
+      </StyledButtonGroup>
+    </Container>
+  );
 };
